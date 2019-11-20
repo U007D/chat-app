4f6864483ede7be10bd65bd9416ba1c5261badae
marianne.goldin <marianne@transparentinc.co>
2019-11-20 15:34:35 -0800

    Get args

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
+use std::env;
 type Result<T, E = Error> = std::result::Result<T, E>;
+#[derive(Debug)]
 enum Error {}
-impl std::error::Error for Error{} // implement trait for type, Error type is now impls std error trait
+//impl std::error::Error for Error {} // implement trait for type, Error type is now impls std error trait
 
 fn main() -> Result<()> {
-    println!("Hello, world!");
+    println!("Hello, {:?}", env::args().next());
     Ok(())
 }
