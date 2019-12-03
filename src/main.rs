27e16a468ab5e9a47c386c7dadd37669fdd7cc90
Marianne Goldin <marianne@transparentinc.co>
2019-12-03 14:57:17 -0800

    Read in name and handle missing arg error

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 use std::env;
-type Result<T, E = Error> = std::result::Result<T, E>;
+use crate::MyError::MissingNameArg;
+
+type Result<T, E = MyError> = std::result::Result<T, E>;
 #[derive(Debug)]
-enum Error {}
-//impl std::error::Error for Error {} // implement trait for type, Error type is now impls std error trait
+enum MyError {
+    MissingNameArg
+}
+//FYI to self impl std::error::Error for Error {} // implement trait for type, Error type is now impls std error trait
 
 fn main() -> Result<()> {
-    println!("Hello, {:?}", env::args().next());
+    println!("Hello, {:?}", env::args().nth(1).ok_or(MissingNameArg)?);
     Ok(())
 }
