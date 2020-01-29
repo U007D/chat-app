302627680efb090930559507a2cf0730b3aa5e8b
Jake Casaletto <jake.casaletto@tpfs.io>
2020-01-29 15:18:04 -0800

    start ping test and add comment w rs docs

--- src/app/unit_tests.rs
+++ src/app/unit_tests.rs
@@ -9,16 +9,6 @@ pub enum Message {
     Ping,
     Hello
 }
-//
-//fn ping(mut socket: tungstenite::WebSocket<Stream::Plain(String)>) -> Result<String> {
-//    socket.write_message(Message::Text("ping".into()))?;
-//    match socket.read_message()? {
-//        Message::Text(s) => Ok(s),
-//        _ => Err(Error::NonTextMessageReceived),
-//    }
-//}
-
-// TODO - convert the Stream to Tls encryption
 
 #[test]
 fn start__app_starts() -> Result<()> {
@@ -27,23 +17,23 @@ fn start__app_starts() -> Result<()> {
     let sut = App::start;
 
     // When
-    let app = sut()?;
+    let actual_app = sut()?;
 
     // Then
-    assert_eq!(app.local_socket, expected_socket);
+    assert_eq!(actual_app.local_socket, expected_socket);
     Ok(())
 }
 
-//#[test]
-//fn ping__live_socket_replies_with_pong() {
-//    // Given
-//    let app = App::new();
-//    let app_socket = app.socket();
-//
-//    // When
-//    let res = ping(app_socket);
-//
-//    // Then
-//    assert_eq!(res, Ok("pong".to_string()));
-//}
+// https://github.com/housleyjk/ws-rs/blob/master/examples/client.rs
+#[test]
+fn ping__live_socket_replies_with_pong() {
+    // Given
+    let expected_socket = SocketAddr::from(([127,0,0,1], 4444));
+    let app = App::start()?;
+    let listner_socket = app.local_socket;
+
+    // When
+
+    // Then
+}
 
